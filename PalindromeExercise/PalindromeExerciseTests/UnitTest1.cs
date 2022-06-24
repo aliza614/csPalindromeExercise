367403fa012204abfb4ad920400e74b066c01c3a
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-24 10:35:22 -0500

    red done

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("hello", false)]
+        public void TestIsAPalindrome(String s, bool expected)
         {
+            //Arrange
+            WordSmith wordSmith = new WordSmith();
 
+            //Act
+            var actual=wordSmith.IsAPalindrome(s);
+            
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
+    //Red-run all tests DONE
+    //green-Implement
 }
