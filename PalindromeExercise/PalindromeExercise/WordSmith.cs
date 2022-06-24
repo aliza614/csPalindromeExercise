7c0b5b982459917f4f2a29d7a3afc1edf045c05f
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-24 10:30:19 -0500

    created IsAPalindrome()

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
