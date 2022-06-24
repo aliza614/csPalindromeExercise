0c89988ee6548f369ceb9d7f6b92e8e57250f393
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-24 14:00:08 -0500

    refactor with Linq

--- PalindromeExercise/PalindromeExercise/WordSmith.cs
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace PalindromeExercise
 {
     public class WordSmith
     {
         public bool IsAPalindrome(string s)
-        {   s=s.ToLower();
+        {   /*s=s.ToLower();
             for(int i = 0; i < s.Length/2; i++)
             {
                 if (s[i] != s[s.Length-1-i]) return false;
             }
-            return true;
+            return true;*/
+
+            return s.ToLower().SequenceEqual(s.ToLower().Reverse());
         }
     }
 }
