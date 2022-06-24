8bedbcdcb77adec49d08e8e6073b76e4fee13084
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-24 13:51:57 -0500

    adding to lower to account for different cases

--- PalindromeExercise/PalindromeExercise/WordSmith.cs
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -7,7 +7,7 @@ namespace PalindromeExercise
     public class WordSmith
     {
         public bool IsAPalindrome(string s)
-        {
+        {   s=s.ToLower();
             for(int i = 0; i < s.Length/2; i++)
             {
                 if (s[i] != s[s.Length-1-i]) return false;
